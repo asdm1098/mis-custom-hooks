68ab123894e8dff3ec5a2f468919b487d95121e1
Stiven Diaz <Stivendiaz98@gmail.com>
2020-10-17 10:48:17 -0500

    primer commmit

---
+++ useForm.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+/*
+Nuestro useForm recibe un objeto, que tiene las propiedades como un campo de texto,
+un selector, o un campo en mi formulario, el cual yo quiero manipular y tambien tengo
+el handleInputChange que me va servir a mi para leerlo rapidamente.
+*/
+
+
+export const useForm = ( initialState = {} ) => {
+   
+    const [values, setValues] = useState(initialState);
+
+    const reset = () => {
+        setValues( initialState );
+    }
+
+    const handleInputChange = ({ target }) => {
+        setValues({
+            ...values,
+            [ target.name ]: target.value
+        });
+    }
+
+    return [ values, handleInputChange, reset ];
+
+}
